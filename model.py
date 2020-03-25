92180e35ef7f382df264a69c106a2d70810370a7
Oliver Proud <owproud@gmail.com>
2020-03-25 14:55:43 +0000

    update for distilbert

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
-from transformers import BertModel, BertTokenizer, BertForSequenceClassification
+from transformers import DistilBertModel, DistilBertTokenizer, DistilBertForSequenceClassification
+# from transformers import BertModel, BertTokenizer, BertForSequenceClassification
 from pathlib import Path
 import torch.nn as nn
 import pandas as pd
@@ -6,12 +7,15 @@ import requests
 import torch
 import os
 
-MODELS = [(BertModel, BertTokenizer, 'bert-base-uncased')]
+# BERT_MODEL = [(BertModel, BertTokenizer, 'bert-base-uncased')]
+DISTILBERT_MODEL = [(DistilBertModel, DistilBertTokenizer, 'distilbert-base-cased')]
 
-for model_class, tokenizer_class, pretrained_weights in MODELS:
+for model_class, tokenizer_class, pretrained_weights in DISTILBERT_MODEL:
 
     tokenizer = tokenizer_class.from_pretrained(pretrained_weights)
     bert_model = model_class.from_pretrained(pretrained_weights)
+
+    print(pretrained_weights)
 
 labels_list = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
 
@@ -20,7 +24,10 @@ class linear_model(nn.Module):
         super().__init__()
 
         embed_size = bert_model.config.hidden_size
-        dropout_prob = bert_model.config.hidden_dropout_prob
+        if pretrained_weights == 'distilbert-base-cased':
+            dropout_prob = bert_model.config.dropout
+        else:
+            dropout_prob = bert_model.config.hidden_dropout_prob
 
         self.bert = bert_model
 
@@ -51,16 +58,26 @@ class linear_model(nn.Module):
 
         return logits
 
-s3_model_url = 'https://toxic-model.s3.eu-west-2.amazonaws.com/toxic_model.pt'
-path = "./model/"
+def download_model(s3_url, model_name):
+    path = "./model/"
+    path_to_model = os.path.join(path, model_name)
+    if not os.path.exists(path_to_model):
+        print("Model weights not found, downloading from S3...")
+        os.makedirs(os.path.join(path), exist_ok=True)
+        filename = Path(path_to_model)
+        r = requests.get(s3_url)
+        filename.write_bytes(r.content)
 
-path_to_model = os.path.join(path, 'toxic_model.pt')
-if not os.path.exists(path_to_model):
-    print("Model weights not found, downloading from S3...")
-    os.makedirs(os.path.join(path), exist_ok=True)
-    filename = Path(path_to_model)
-    r = requests.get(s3_model_url)
-    filename.write_bytes(r.content)
+    return path_to_model
+
+if pretrained_weights == 'distilbert-base-cased':
+    s3_url = 'https://toxic-model.s3.eu-west-2.amazonaws.com/distil_toxic_model.pt'
+    model_name = 'distil_toxic_model.pt'
+else:
+    s3_url = 'https://toxic-model.s3.eu-west-2.amazonaws.com/toxic_model.pt'
+    model_name = 'toxic_model.pt'
+
+path_to_model = download_model(s3_url, model_name)
 
 model = linear_model(bert_model, len(labels_list))
 model.load_state_dict(torch.load(path_to_model, map_location=torch.device('cpu')))
